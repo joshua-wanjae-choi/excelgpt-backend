19150e8ada25bc7bca667203cf99a17d3307c6c1
joshua-wanjae-choi <joshua.wanjae.choi@gmail.com>
2023-11-14 22:42:51 +0900

    환경변수로 설정값 지정하도록 수정.

--- api/config/config.py
+++ api/config/config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
-    limit_query_count = 10000
-    days_expired_files = 1
-    disk_dir_name = "disk"
+    limit_query_count = int(os.getenv('LIMIT_QUERY_COUNT', "100"))
+    days_expired_files = int(os.getenv('DAYS_EXPIRED_FILES', "1"))
+    disk_dir_name = os.getenv('DISK_DIR_NAME', "disk")
